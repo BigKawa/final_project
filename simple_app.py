f45882ba6dc83de4854596b55cb2dd32be9f8e01
BigKawa <164774630+BigKawa@users.noreply.github.com>
2024-11-11 17:25:35 +0100

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,9 @@
 
 import streamlit as st
+import pandas as pd
+import numpy as np
+import functions as py  # Import the functions module
+
+
 
 st.title("📊 Automated Financial Report Analysis Tool")
