6c1600805ca1674681106fe2312cd66cdba5c30f
BigKawa <164774630+BigKawa@users.noreply.github.com>
2024-11-13 15:15:39 +0100

    devloping streamlit

--- simple_app.py
+++ simple_app.py
@@ -19,6 +19,8 @@ bs_annual = pd.read_csv("Data/bs_annual_MSFT.csv")
 cf_annual = pd.read_csv("Data/cf_annual_MSFT.csv")
 pnl_annual = pd.read_csv("Data/pnl_annual_MSFT.csv")
 
+
+# Testing
 bs_annual, pnl_annual, cf_annual = t.clean_dataframes(bs_annual, pnl_annual, cf_annual)
 bs_annual, pnl_annual, cf_annual = t.calculate_kpis(bs_annual, pnl_annual, cf_annual)
 pnl_annual_prev, bs_annual_prev, cf_annual_prev = t.create_previous_year_dataframes(bs_annual, pnl_annual, cf_annual)
@@ -29,6 +31,23 @@ pnl_concat, bs_concat, cf_concat = t.generate_insights(pnl_concat, bs_concat, cf
 print(pnl_concat.info())
 
 st.title("📊 Automated Financial Report Analysis Tool")
+st.caption("This is a simple app to analyze the financial report of a company using Streamlit and Python. Please enter the company symbol")
 
+
+
+st.header("The text insider st.header() is in header formatting")
+st.subheader("The text insider st.subheader() is in subheader formatting")
+
+
+st.code("python") # Use st.code() to display code snippets
+
+
+st.header("This is the balance sheet")
+st.caption("Diplay Dataframe as an interactive table")
+st.dataframe(data= bs_concat)
+
+
+st.header("This is the Profit and Loss sheet")
+st.caption("Diplay Dataframe as an interactive table")
 st.write(pnl_concat)
 
