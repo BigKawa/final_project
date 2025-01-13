18f2917f95511be8cf852850873e2a683f334f93
BigKawa <164774630+BigKawa@users.noreply.github.com>
2025-01-13 18:50:56 +0100

    Upload csv files and streamlit

--- run_streamlit.py
+++ run_streamlit.py
@@ -13,3 +13,5 @@ mac_path = "/Users/linh/Documents/GitHub/GroupWork/Final_Project/final_project"
 os.chdir("/Users/linh/Documents/GitHub/GroupWork/Final_Project/final_project")
 # Replace 'your_script.py' with the actual name of your Streamlit file
 os.system("streamlit run simple_app.py")
+
+
